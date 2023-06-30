0930e5efe5a22748a4009102abafd4feff85612a
Jinal Tandel <jinaltandel25@gmail.com>
2023-06-30 12:31:39 +0100

    React Portfolio website files

---
+++ src/data.js
@@ -0,0 +1,60 @@
+export const projects = [
+  {
+    title: "ASP.Net Core, C#, HTML5, SCSS",
+    subtitle: "Enex Group Limited",
+    description:
+      "This website has been created using ASP.NET core v6.0. Also, Node.js for NPM and Gulp are used for compiling client side assets (SCSS, JS). The site has a /news section which is driven by a CMS called Piranha CMS . All other sections of the site are static.",
+    image: "./projects/enex-group.png",
+    link: "https://enex-group.com/",
+  },
+  {
+    title: "React and HTML5",
+    subtitle: "React Portfolio",
+    description:
+      "This is an example of my work using React. First website where I integrated and practiced React. In this project, Tailwind.CSS is used for designing the web page. Tailwind is a ready-to-use CSS with fully-responsive UI components. ",
+    image: "./projects/my-portfolio.png",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "ASP.NET Web Forms and Bespoke CMS",
+    subtitle: "Burgoynes - Consulting Scientists and Engineers",
+    description:
+      "Burgoynes website is a Content Management System (CMS) based website. Built with bespoke CMS using ASP.NET Web forms, HTML5, Bootstrap, JavaScript and CSS.",
+    image: "./projects/burgoynes.png",
+    link: "https://burgoynes.com/",
+  },
+  {
+    title: "ASP.NET Core, Piranha CMS, Bootstrap and SCSS",
+    subtitle: "The Alternative Steel Company",
+    description:
+      "This website has been created using ASP.NET core v6.0. Also, Node.js for NPM and Gulp are used for compiling client side assets (SCSS, JS). The site has a /news section which is driven by a CMS called Piranha CMS . All other sections of the site are static.",
+    image: "./projects/alternative-steel.png",
+    link: "https://www.alternativesteelco.co.uk/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "/testimonials/lego-3.jpg",
+    name: "Hermoine Ganger",
+    company: "York House",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "/testimonials/lego-6.jpg",
+    name: "Ronald Weasley",
+    company: "Metal Industries",
+  },
+];
+
+export const skills = [
+  "HTML5",
+  "CSS/SCSS/Bootstrap",
+  "React",
+  "JavaScript/JQuery",
+  "ASP.NET V4+ and Core",
+  "C#",
+];
